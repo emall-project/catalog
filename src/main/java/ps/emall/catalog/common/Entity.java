1c2de7847d59f9229313a4e009d77b102c30d950
jehadhamid <jehadadhame@gmail.com>
2026-04-23 22:30:37 +0300

    validate-media-usage-across-all-related-entities

--- src/main/java/ps/emall/catalog/common/Entity.java
+++ src/main/java/ps/emall/catalog/common/Entity.java
@@ -1,12 +1,14 @@
 package ps.emall.catalog.common;
 
+import ps.emall.catalog.category.audience_config.CategoryAudienceConfig;
+
 public enum Entity {
     CATEGORY,
+    CategoryAudienceConfig,
     BRAND,
     TAG,
     ATTRIBUTE,
     PRODUCT,
     PRODUCT_VARIANT,
-    PRODUCT_MEDIA
-
+    PRODUCT_MEDIA,
 }
