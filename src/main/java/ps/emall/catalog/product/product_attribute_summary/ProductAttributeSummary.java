ef264636b7f0fb6242bbd2046ed75dfecaf3a640
jehadhamid <jehadadhame@gmail.com>
2026-05-05 10:14:19 +0300

    add build image

---
+++ src/main/java/ps/emall/catalog/product/product_attribute_summary/ProductAttributeSummary.java
@@ -0,0 +1,15 @@
+package ps.emall.catalog.product.product_attribute_summary;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductAttributeSummary {
+
+
+}
