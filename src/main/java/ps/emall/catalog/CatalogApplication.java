595b3701f08cb97509473362e5f4df57833899f3
jehadhamid <jehadadhame@gmail.com>
2026-05-07 13:13:01 +0300

    Use Palestine Gaza timezone

--- src/main/java/ps/emall/catalog/CatalogApplication.java
+++ src/main/java/ps/emall/catalog/CatalogApplication.java
@@ -5,12 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import ps.emall.catalog.config.FeignConfig;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableFeignClients(defaultConfiguration = FeignConfig.class)
 public class CatalogApplication {
 
+    private static final String APPLICATION_TIME_ZONE = "Asia/Gaza";
+
     public static void main(String[] args) {
 
+        TimeZone.setDefault(TimeZone.getTimeZone(APPLICATION_TIME_ZONE));
         SpringApplication.run(CatalogApplication.class, args);
     }
 
