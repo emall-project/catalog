f91ce32a8a426a3f115433650d0247811afcfdb1
jehadhamid <jehadadhame@gmail.com>
2026-05-08 11:47:59 +0300

    update media manager getById list method

--- src/main/java/ps/emall/catalog/client/media_manager/MediaManagerClient.java
+++ src/main/java/ps/emall/catalog/client/media_manager/MediaManagerClient.java
@@ -22,6 +22,6 @@ public interface MediaManagerClient {
     @GetMapping("/internal/files/{id}")
     MediaResponse<FileDto> getById(@PathVariable("id") UUID id);
 
-    @PostMapping("internal/files/list")
+    @PostMapping("/internal/files/list")
     MediaResponse<List<FileDto>> getByIds(@RequestBody List<UUID> ids);
 }
